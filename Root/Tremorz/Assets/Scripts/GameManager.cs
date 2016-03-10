bc50abc6e3ba65bed0f1c40d4bd3f5aef12884f1
MetaJen <meta.gen1419@gmail.com>
2016-03-09 22:17:36 -0800

    Menus & Game Timer
    Added canvases for pause menu and game over menu. I haven't hooked them
    up as of yet. Added a HUD canvas and put the game timer on it. Counts
    down. Will tweek it and have it pop up the game over menu when it hits
    0.

--- Root/Tremorz/Assets/Scripts/GameManager.cs
+++ Root/Tremorz/Assets/Scripts/GameManager.cs
@@ -4,21 +4,34 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public float gameTime;
+    public float gameTime = 5.00f;
+    public Text clockText;
 
     public Canvas gameOver;
     public Canvas pauseMenu;
 
     void Start()
     {
+        clockText = clockText.GetComponent<Text>();
+
         gameOver = gameOver.GetComponent<Canvas>();
         pauseMenu = pauseMenu.GetComponent<Canvas>();
         gameOver.enabled = false;
         pauseMenu.enabled = false;
+
+
     }
 
     void Update()
     {
+        gameTime -= Time.deltaTime;
+        clockText.text = gameTime.ToString( "f2" );
+        print( gameTime );
+    }
 
+    public void GameOver()
+    {
+        if( gameTime == 0 )
+            gameOver.enabled = true;
     }
 }
